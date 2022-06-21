a19ab1ee271c8d853e3b7a3d17812ca0631cb792
Lam Jia Seng <jia-seng.lam@bassnet.no>
2022-06-21 22:39:03 +0800

    spin docker using docker compose

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h2>Hi There!</h2>');
+  res.send('<h2>Hi There</h2>');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
